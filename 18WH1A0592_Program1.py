aaa766c34f7a1b3c6088db0fa202a8fb58a7c45e
Zainab Hameed <68607453+zainabhameed@users.noreply.github.com>
2021-04-28 15:53:28 +0530

    Add files via upload

---
+++ 18WH1A0592_Program1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+probAbsentFriday=0.03
+probFriday=0.2
+bayesResult=(probAbsentFriday/probFriday)
+print(bayesResult * 100)
+
+
+# In[ ]:
+
+
+
+
